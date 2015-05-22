95e474c61111bcc05ea2e2279b7c5b2f9bd092d8
jepio <jeremi.piotrowski@gmail.com>
2015-05-22 19:33:04 +0200

    Expose first part of the fcd and hidapi functionality

--- pyfcd.pyx
+++ pyfcd.pyx
@@ -1,10 +1,24 @@
 '''Cython wrapper around the fcdctl command-line utility functionality.'''
 
 cdef extern from "fcd.h":
-    pass
+    int whichdongle
+    const unsigned int _usVID
+    const unsigned int _usPID
+
+    ctypedef enum FCD_MODE_ENUM:
+        FCD_MODE_NONE
+        FCD_MODE_BL
+        FCD_MODE_APP
+
+    FCD_MODE_ENUM fcdGetMode()
+    FCD_MODE_ENUM fcdGetFwVerStr(char *str)
 
 cdef extern from "hidapi.h":
-    pass
+    struct hid_device_info:
+        hid_device_info *next
+
+    hid_device_info *hid_enumerate(unsigned short vendor_id,
+                                   unsigned short product_id)
 
 cdef extern void c_print_list "print_list" ()
 cdef extern void c_print_help "print_help" ()
