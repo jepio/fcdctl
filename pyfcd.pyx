b99ce7f81e6609649259b6121767d08e790143e1
jepio <jeremi.piotrowski@gmail.com>
2015-05-22 19:32:06 +0200

    Add module and function docstrings

--- pyfcd.pyx
+++ pyfcd.pyx
@@ -1,3 +1,5 @@
+'''Cython wrapper around the fcdctl command-line utility functionality.'''
+
 cdef extern from "fcd.h":
     pass
 
@@ -8,7 +10,10 @@ cdef extern void c_print_list "print_list" ()
 cdef extern void c_print_help "print_help" ()
 
 def print_list():
+    '''Print list of all Funcube Dongles.'''
     c_print_list()
 
+
 def print_help():
+    '''Print the help string for the fcdctl app.'''
     c_print_help()
